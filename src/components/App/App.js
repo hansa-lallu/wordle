e34f4e8ff6e3940252da8c796aeb0253710b121b
hansa-lallu <hansalallu@gmail.com>
2024-01-08 16:14:07 +0000

    missed commit including styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,9 @@ import Game from '../Game';
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
       <Header />
-      <div>
+      <div className="game-wrapper">
         <Game />
       </div>
     </div>
