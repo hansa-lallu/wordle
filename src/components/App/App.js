a2a0fb31edf19b694ff94813695c977d4a88e06c
hansa-lallu <hansalallu@gmail.com>
2024-01-08 11:08:21 +0000

    add Game component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import Header from '../Header';
+import Game from '../Game';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div>
+        <Game />
+      </div>
     </div>
   );
 }
---
+++ src/components/Game/Game.js
@@ -0,0 +1,16 @@
+import { sample } from '../../utils';
+import { WORDS } from '../../data';
+import GuessInput from '../GuessInput';
+
+const answer = sample(WORDS);
+
+console.info({ answer });
+
+function Game() {
+  return (
+    <>
+      <GuessInput />
+    </>);
+}
+
+export default Game;
