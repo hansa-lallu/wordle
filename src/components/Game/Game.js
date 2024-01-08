e4a103b75ab5c7f4e6dfa33a19fae74a5f637e60
hansa-lallu <hansalallu@gmail.com>
2024-01-08 12:26:59 +0000

    Add Guess Input component to list all guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,16 +1,26 @@
+import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 
 const answer = sample(WORDS);
 
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess) {
+    setGuesses([...guesses, tentativeGuess]);
+  }
+
   return (
     <>
-      <GuessInput />
-    </>);
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
+    </>
+  );
 }
 
 export default Game;
